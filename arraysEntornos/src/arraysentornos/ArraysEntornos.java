a64b8ec00d52b0e00e44e4581bae9ae852b0bf28
miguel <miguel@192.168.1.148>
2021-01-31 14:13:17 +0100

    Ejercicio 3- v3

--- arraysEntornos/src/arraysentornos/ArraysEntornos.java
+++ arraysEntornos/src/arraysentornos/ArraysEntornos.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package arraysentornos;
+
 import java.util.*;
+
 /**
  *
  * @author miguel
@@ -17,16 +19,38 @@ public class ArraysEntornos {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        int numeros []= new int [10];
+        int contador=0;
         
+        System.out.println("¿Cuantos numeros vas a leer?");
+        contador=sc.nextInt();
         
+        int numeros[] = new int[contador];
+        if(contador<10){
+            System.out.println("No puedes superar el maximo de 10 numeros leidos, introducelo de nuevo");
+            contador=sc.nextInt();
+        }else{
+            
+        for (int i = 0; i < contador; i++) {
+            System.out.println("Introduce numeros");
+            numeros[i] = sc.nextInt();
+
+                for (int j = 0; j < i; j++) {
+                    if(numeros[j]==numeros[i]){
+                        System.out.println("El numero ya esta introducido");
+                        i--;
+                    }
+                    
+                }
+            }
+        }
+
+        Arrays.sort(numeros);
+        System.out.println("Los numeros introducidos son ");
         for (int i = 0; i < numeros.length; i++) {
-        System.out.println("Introduce 10 numeros");
-        numeros[i]=sc.nextInt();
+            System.out.println(numeros[i]);
         }
         
-        Arrays.sort(numeros);
-    
+       } 
+
     }
-    
-}
+
